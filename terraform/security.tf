133755f3068ce634e0777271c503403f3bc3c1c7
Vector <mac@Vector-2.local>
2026-02-14 17:36:21 +0100

    update port 22 cidr block

--- terraform/security.tf
+++ terraform/security.tf
@@ -15,6 +15,13 @@ resource "aws_default_security_group" "default-sg"{
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     
     egress {
         from_port = 0
